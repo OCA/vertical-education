86f3abec119fe574ca39b6a7c73a4a4fb35d8111
Angel Moya Pardo <amoyapardo@gmail.com>
2017-08-04 17:50:52 +0200

    MIG facility module

--- education_facility/__manifest__.py
+++ education_facility/__manifest__.py
@@ -1,48 +1,28 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
-#    Tech-Receptives Solutions Pvt. Ltd.
-#    Copyright (C) 2009-TODAY Tech-Receptives(<http://www.techreceptives.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# Copyright 2009-TODAY Tech-Receptives(<http://www.techreceptives.com>)
+# Copyright 2017 Pesol (<http://pesol.es>)
+#                Angel Moya <angel.moya@pesol.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
 {
     'name': 'Education Facility',
-    'version': '10.0.3.0.0',
+    'version': '10.0.1.0.0',
     'license': 'LGPL-3',
     'category': 'Education',
     "sequence": 3,
     'summary': 'Manage Facility',
     'complexity': "easy",
-    'author': 'Tech Receptives',
-    'website': 'http://www.education.org',
+    'author': 'Tech Receptives, PESOL, Odoo Community Association (OCA)',
     'depends': ['education'],
     'data': [
         'views/facility_view.xml',
         'views/facility_line_view.xml',
         'security/ir.model.access.csv',
-        'facility_menu.xml',
     ],
     'demo': [
         'demo/facility_demo.xml'
     ],
-    'images': [
-        'static/description/education_facility_banner.jpg',
-    ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
-    'application': True,
+    'application': False,
 }
