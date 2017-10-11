a35ce45467e200e57c477231bb57b010be139164
Angel Moya Pardo <amoyapardo@gmail.com>
2017-10-11 20:26:24 +0200

    FIX views

--- education/models/teacher.py
+++ education/models/teacher.py
@@ -10,17 +10,3 @@ class EducationTeacher(models.Model):
     partner_id = fields.Many2one(
         'res.partner', 'Partner', required=True, ondelete="cascade")
 
-    employee_id = fields.Many2one(
-        comodel_name='hr.employee',
-        string='Employee')
-
-    @api.multi
-    def create_employee(self):
-        for record in self:
-            vals = {
-                'name': record.name,
-                'address_home_id': record.partner_id.id
-            }
-            employee_id = self.env['hr.employee'].create(vals)
-            record.write({'emp_id': employee_id.id})
-            record.partner_id.write({'supplier': True, 'employee': True})
