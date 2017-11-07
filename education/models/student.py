c65082e73e282903905057eb56db610f5911aad3
Luis Adan JH <luis.jimenez@pesol.es>
2017-11-07 19:00:40 +0100

    WIP

--- education/models/student.py
+++ education/models/student.py
@@ -19,3 +19,13 @@ class EducationStudent(models.Model):
         result = action.read()[0]
         result['domain'] = [('partner_id', '=', self.partner_id.id)]
         return result
+
+    def schedule_meeting(self):
+        partner_ids = self.ids
+        partner_ids.append(self.env.user.partner_id.id)
+        action = self.env.ref('calendar.action_calendar_event').read()[0]
+        action['context'] = {
+            'search_default_partner_ids': self._context['partner_name'],
+            'default_partner_ids': partner_ids,
+        }
+        return action
