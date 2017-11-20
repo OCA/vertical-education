d600f8bc45cc02700a998d9bae422d727d044078
Luis Adan JH <luis.jimenez@pesol.es>
2017-11-20 18:56:32 +0100

    WIP

--- education_timetable/models/session.py
+++ education_timetable/models/session.py
@@ -5,6 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from odoo import models, api, fields
+from datetime import datetime
 
 
 class EducationSession(models.Model):
@@ -22,6 +23,14 @@ class EducationSession(models.Model):
     date = fields.Date(
         string='Date')
 
+    start_time = fields.Datetime(
+        string='Start time',
+        compute='_compute_time')
+
+    end_time = fields.Datetime(
+        string='End time',
+        compute='_compute_time')
+
     state = fields.Selection(
         [('draft', 'Draft'),
          ('done', 'Done')],
@@ -33,9 +42,45 @@ class EducationSession(models.Model):
         inverse_name='session_id',
         string='Ausences')
 
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string='Salesperson',
+        index=True,
+        track_visibility='onchange',
+        default=lambda self: self.env.user)
+
+    teacher_id = fields.Many2one(
+        comodel_name='education.teacher',
+        related='timetable_id.teacher_id',
+        string='Teacher')
+
+    subject_id = fields.Many2one(
+        comodel_name='education.subject',
+        related='timetable_id.subject_id',
+        string='Subject')
+
+    group_id = fields.Many2one(
+        comodel_name='education.group',
+        related='timetable_id.group_id',
+        string='Group')
+
     @api.model
     def create(self, vals):
         if vals.get('code', 'New') == 'New':
             vals['code'] = self.env['ir.sequence'].next_by_code(
                 'education.session') or 'New'
         return super(EducationSession, self).create(vals)
+
+    @api.depends('date',
+                 'timetable_id.timerange_id.start_time',
+                 'timetable_id.timerange_id.end_time',
+                 )
+    def _compute_time(self):
+        for session in self:
+            start_time = str(session.timetable_id.timerange_id.start_time).split('.')
+            end_time = str(session.timetable_id.timerange_id.end_time).split('.')
+            date = session.date.split('-')
+            session.start_time = datetime(int(date[0]), int(
+                date[1]), int(date[2]), int(start_time[0])-1, int(start_time[1]), 0)
+            session.end_time = datetime(int(date[0]), int(
+            date[1]), int(date[2]), int(end_time[0])-1, int(end_time[1]), 0)
