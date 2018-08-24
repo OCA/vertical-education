9077ccc9e158b3e657c04ea0b70763ca5b1b41a5
Luis Adan JH <luis.jimenez@pesol.es>
2018-08-24 12:53:37 +0200

    Buttons for partners and groups in base

--- education/models/partner.py
+++ education/models/partner.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, _
 
 
 class ResPartner(models.Model):
@@ -12,3 +12,35 @@ class ResPartner(models.Model):
         comodel_name='education.record',
         inverse_name='student_id',
         string='Academic Records')
+
+    def open_student_enrolled_groups(self):
+        return {
+            'name': _("Academic Groups"),
+            'view_mode': 'tree,form',
+            'res_model': 'education.group',
+            'src_model': 'res.partner',
+            'type': 'ir.actions.act_window',
+            'domain': '[("enrollment_ids.student_id", "=", active_id)]',
+            'context': '{"search_default_state": "active"}'
+        }
+
+    def open_teacher_active_groups(self):
+        return {
+            'name': _("Academic Groups"),
+            'view_mode': 'tree,form',
+            'res_model': 'education.group',
+            'src_model': 'res.partner',
+            'type': 'ir.actions.act_window',
+            'domain': '[("tutor_id", "=", active_id)]',
+            'context': '{"search_default_state": "active"}'
+        }
+
+    def open_student_enrollments(self):
+        return {
+            'name': _("Enrollments"),
+            'view_mode': 'tree,form',
+            'res_model': 'education.enrollment',
+            'src_model': 'res.partner',
+            'type': 'ir.actions.act_window',
+            'domain': '[("student_id", "=", active_id)]'
+        }
